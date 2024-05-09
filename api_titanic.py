3f07d9b139690ce0b417cd804bdd89afa6ba2857
corciatheo <theo.corcia@student-cs.fr>
2024-05-09 14:55:28 +0100

    version2.1

--- api_titanic.py
+++ api_titanic.py
@@ -2,9 +2,11 @@ import joblib
 import uvicorn
 from fastapi import FastAPI
 import pandas as pd
+from prometheus_client import make_asgi_app
 
 app = FastAPI()
-
+metrics_app=make_asgi_app()
+app.mount("/metrics",metrics_app)
 
 @app.post("/titanic")
 def prediction_api(pclass: int, sex: int, age: int) -> bool:
@@ -16,6 +18,8 @@ def prediction_api(pclass: int, sex: int, age: int) -> bool:
     prediction = titanic_model.predict(pd.DataFrame(x).transpose())
 
     return prediction[0] == 1
+
+
 
 
 if __name__ == "__main__":
